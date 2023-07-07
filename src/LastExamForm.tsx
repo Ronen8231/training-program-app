ab1fca0d5cd57594bcd08590042c8eb2178f0d3e
Lior <lior.vallach@gmail.com>
2023-07-07 15:27:13 +0300

    small ui fixes

--- src/LastExamForm.tsx
+++ src/LastExamForm.tsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import * as React from "react";
 import Accordion from "react-bootstrap/Accordion";
+import './App.css';
 
 interface WorkoutProps {
     totalTime: number;
@@ -18,17 +19,19 @@ function ContinuousWorkouts({ totalTime }: WorkoutProps) {
     let oneKmPace = totalTime / 3;
     return (
         <>
-            <h3>אימוני רצף</h3>
-            <Accordion defaultActiveKey="0">
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>אימון רצף נרחב</Accordion.Header>
-                    <Accordion.Body>רוץ עד 5 קילומטר בקצב {paceAsMinutes(oneKmPace * 1.4)} לקילומטר </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>אימון רצף בינוני קבוע</Accordion.Header>
-                    <Accordion.Body>רוץ עד 3 קילומטר בקצב {paceAsMinutes(oneKmPace * 1.15)} לקילומטר</Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
+            <div className="workoutPlanContainer">
+                <h3>אימוני רצף</h3>
+                <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>אימון רצף נרחב</Accordion.Header>
+                        <Accordion.Body>רוץ עד 5 קילומטר בקצב {paceAsMinutes(oneKmPace * 1.4)} לקילומטר </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>אימון רצף בינוני קבוע</Accordion.Header>
+                        <Accordion.Body>רוץ עד 3 קילומטר בקצב {paceAsMinutes(oneKmPace * 1.15)} לקילומטר</Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+            </div>
         </>
     );
 }
@@ -37,24 +40,26 @@ function IntervalWorkouts({ totalTime }: WorkoutProps) {
     let oneKmPace = totalTime / 3;
     return (
         <>
-            <h3>אימוני מקטעים</h3>
-            <Accordion defaultActiveKey="0">
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>אימון מקטעים - פירמידה יורדת</Accordion.Header>
-                    <Accordion.Body>
-                        רוץ 800 מטרים בקצב של {paceAsMinutes(oneKmPace * 1.05 * 0.8)} דקות - נוח 2 דקות <br></br>
-                        רוץ 600 מטרים בקצב של {paceAsMinutes(oneKmPace * 1.05 * 0.6)} דקות - נוח דקה וחציּ <br></br>
-                        רוץ 400 מטרים בקצב של {paceAsMinutes(oneKmPace * 1 * 0.4)} דקות - נוח דקה <br></br>
-                        רוץ 200 מטרים בקצב של {paceAsMinutes(oneKmPace * 1 * 0.2)} דקות <br></br>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>אימון מקטעים - מקטעים קבועים</Accordion.Header>
-                    <Accordion.Body>
-                        רוץ 4*500 מטרים בקצב של {paceAsMinutes(oneKmPace * 1 * 0.5)} - נוח דקה וחצי בין המקטעים
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
+            <div className="workoutPlanContainer">
+                <h3>אימוני מקטעים</h3>
+                <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>אימון מקטעים - פירמידה יורדת</Accordion.Header>
+                        <Accordion.Body>
+                            רוץ 800 מטרים בקצב של {paceAsMinutes(oneKmPace * 1.05 * 0.8)} דקות - נוח 2 דקות <br></br>
+                            רוץ 600 מטרים בקצב של {paceAsMinutes(oneKmPace * 1.05 * 0.6)} דקות - נוח דקה וחציּ <br></br>
+                            רוץ 400 מטרים בקצב של {paceAsMinutes(oneKmPace * 1 * 0.4)} דקות - נוח דקה <br></br>
+                            רוץ 200 מטרים בקצב של {paceAsMinutes(oneKmPace * 1 * 0.2)} דקות <br></br>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>אימון מקטעים - מקטעים קבועים</Accordion.Header>
+                        <Accordion.Body>
+                            רוץ 4*500 מטרים בקצב של {paceAsMinutes(oneKmPace * 1 * 0.5)} - נוח דקה וחצי בין המקטעים
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+            </div>
         </>
     );
 }
@@ -82,14 +87,16 @@ function Calculator() {
                             תוצאת בוחן הכש"ג
                         </label>
                     </h2>
-                    <div id="lastExamResult">
+                    <div id="lastExamResult" className="results">
                         <input className="form-control form-control-sm" onChange={(e) => setHours(Number(e.target.value))} placeholder="שעות" type="number" name="time_hours" min="0" max="59"></input>
                         <input className="form-control form-control-sm" onChange={(e) => setMinutes(Number(e.target.value))} placeholder="דקות" type="number" name="time_minutes" min="0" max="59"></input>
                         <input className="form-control form-control-sm" onChange={(e) => setSeconds(Number(e.target.value))} placeholder="שניות" type="number" name="time_seconds" min="0" max="59"></input>
                     </div>
-                    <button type="submit" className="btn btn-primary" onClick={handleCalculateClick}>
-                        חשב
-                    </button>
+                    <div className="btn-container">
+                        <button type="submit" className="calculate btn btn-primary" onClick={handleCalculateClick}>
+                            חשב
+                        </button>
+                    </div>
                 </div>
             </form>
             {continuousWorkouts}
