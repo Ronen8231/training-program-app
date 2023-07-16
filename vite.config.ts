813b27ef95c6ec314e5babedfd845a369db8dfb5
Ronen Hanukayev <ronenh8231@gmail.com>
2023-07-16 04:10:57 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { fileURLToPath } from 'url';
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/training-program-app/",
-  plugins: [react()],
-})
+    base: "/training-program-app/",
+    plugins: [react()],
+    build: {
+        outDir: "dist",
+
+        rollupOptions: {
+        input: {
+          index: fileURLToPath(new URL('index.html', import.meta.url)),
+          videos: fileURLToPath(new URL('videos.html', import.meta.url))
+
+    }
+    },
+    },
+});
